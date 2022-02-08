6f4f57cad54a87a8d33beb472773604bbccc2051
Bharath Balasubramaniam <79153733+bharath-balasubramaniam@users.noreply.github.com>
2022-02-08 23:26:52 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import mongoose from "mongoose";
+import Cards from "./dbCards.js";
+import Cors from "cors";
+//App config
+const app = express();
+const port = process.env.PORT || 8000;
+const connection_url = `mongodb+srv://bharath:10051995@dating-app-mern.hn47z.mongodb.net/dating-db?retryWrites=true&w=majority`;
+//Middleware
+app.use(express.json());
+app.use(Cors());
+//DB config
+mongoose.connect(connection_url, {
+  useNewUrlparser: true,
+  useUnifiedTopology: true,
+});
+//API endpoints
+app.get("/", (req, res) => {
+  res.status(200).send("Hello Web developer");
+});
+app.post("/dating/cards", (req, res) => {
+  const dbCard = req.body;
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+app.get("/dating/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+//Listener
+app.listen(port, () => console.log(`Listeniing on the port : ${port}`));
